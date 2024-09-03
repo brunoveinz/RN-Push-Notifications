8f7edec175c9389490f8cd521affac6c21819e63
bveinz <velosobruno61@gmail.com>
2024-09-03 11:17:00 -0400

    intentando hacer un push notification con error

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Button, View } from 'react-native';
+import * as Notifications from 'expo-notifications'
 
-export default function App() {
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+      shouldShowAlert: true
+    };
+  }
+});
+
+export default function App() {  
+  function scheduleNotificationHandler(){
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'My first local notification',
+        body: 'This is the body of the notification',
+        data: { username: 'Max'}
+      },
+      trigger: {
+        seconds: 5
+      }
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Helo World, Push Notifications!</Text>
+      <Button title='Schedule Notification' onPress={scheduleNotificationHandler}/>
       <StatusBar style="auto" />
     </View>
   );
